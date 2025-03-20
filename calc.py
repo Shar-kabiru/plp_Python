f0322c062a33b4302c1161a953ac5597b7c2c20a
Shar-kabiru <sharleennjerikabiru@gmail.com>
2025-03-20 13:11:54 +0300

    aeithmetic operations

--- calc.py
+++ calc.py
@@ -1,4 +1,23 @@
-a = 20
-b = 30
-c = 10
-print (a + b - c)
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+    
+operation = input("Enter the operation (+, -, *, /): ")
+    
+if operation == '+':
+        result = num1 + num2
+elif operation == '-':
+        result = num1 - num2
+elif operation == '*':
+        result = num1 * num2
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        print("Error: Division by zero is not allowed.")
+        
+else:
+    print("Error: Invalid operation.")
+
+    
+    # Print the result
+print(f"The result of {num1} {operation} {num2} is: {result}")
